f3681d77812006987739eda67c1365e8f233b266
Alin Eugen Deac <aedart@gmail.com>
2016-10-22 17:10:23 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// Exceptions
+import InvalidTarget from './src/Exceptions/InvalidTarget';
+export { InvalidTarget };
+
+import InvalidData from './src/Exceptions/InvalidData';
+export { InvalidData };
+
+import MetaError from './src/Exceptions/MetaError';
+export { MetaError }
+
+// Meta data
+import Data from './src/Meta/Data';
+export { Data };
+
+import ClassData from './src/Meta/ClassData';
+export { ClassData };
+
+import MethodData from './src/Meta/MethodData';
+export { MethodData }
+
+// Mixins
+import DependenciesAware from './src/Mixins/DependenciesAware';
+export { DependenciesAware };
+
+// Default - Meta
+import Meta from './src/Meta';
+export default Meta;
