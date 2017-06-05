e74bc7517b225cb0b24f932db213e0915b0c58bd
Alin Eugen Deac <aedart@gmail.com>
2017-06-05 15:14:16 +0200

    Fix incorrect mix dependency
    Forgot to use the new package instead!

--- src/Meta/ClassData.js
+++ src/Meta/ClassData.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import Data from './Data';
-import { mix } from 'mixwith/src/mixwith';
+import mix from '@vcas/js-mixin';
 import DependenciesAware from './../Mixins/DependenciesAware';
 
 /**
